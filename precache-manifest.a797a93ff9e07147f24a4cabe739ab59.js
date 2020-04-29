623e13109d251157fc75de2944958975d6adad76
naina001 <naina.17bcs1205@abes.ac.in>
2020-04-29 12:53:30 +0530

    Updates

---
+++ precache-manifest.a797a93ff9e07147f24a4cabe739ab59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdc3a2f4c2914b64a7f00367a1f07f2",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "835ae440a7a08b16cda4",
+    "url": "/Todo-list/static/css/main.2846d2ae.chunk.css"
+  },
+  {
+    "revision": "2be429e78ec82733a226",
+    "url": "/Todo-list/static/js/2.8487b2d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list/static/js/2.8487b2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835ae440a7a08b16cda4",
+    "url": "/Todo-list/static/js/main.bb86b7b3.chunk.js"
+  },
+  {
+    "revision": "9ee4d37adada543ff3e7",
+    "url": "/Todo-list/static/js/runtime-main.fc143649.js"
+  }
+]);
